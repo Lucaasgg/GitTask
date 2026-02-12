8d863f067f22c5a5c2a7f9c0d98bd8ff3f1dd144
Lucas Garcia <lukggpalomo@gmail.com>
2026-02-12 12:39:16 +0100

    modified line 3 in main

--- GitTask/src/demo/main.java
+++ GitTask/src/demo/main.java
@@ -3,7 +3,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("base line");
         System.out.println("conflict solved");
-        System.out.println("Líne 3");
+        System.out.println("modified line 3 in main");
         System.out.println("switching to master");
     }
 }
