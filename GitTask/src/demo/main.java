1724317a00e7ad34d51aa450208e6bb04e08a486
Lucas Garcia <lukggpalomo@gmail.com>
2026-02-12 12:35:40 +0100

    conflicts solved

--- GitTask/src/demo/main.java
+++ GitTask/src/demo/main.java
@@ -2,7 +2,7 @@ package demo;
 public class main {
     public static void main(String[] args) {
         System.out.println("base line");
-        System.out.println("change from master");
+        System.out.println("conflict solved");
         System.out.println("Líne 3");
         System.out.println("switching to master");
     }
