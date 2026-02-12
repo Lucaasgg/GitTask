bd3abb743bfcb7a93a4b30a25d0884bd1c07af15
Lucas Garcia <lukggpalomo@gmail.com>
2026-02-12 12:24:19 +0100

    Merge remote-tracking branch 'GitTask/branch-ex1'

--- GitTask/src/demo/main.java
+++ GitTask/src/demo/main.java
@@ -2,7 +2,7 @@ package demo;
 public class main {
     public static void main(String[] args) {
         System.out.println("base line");
-        System.out.println("Líne 2");
+        System.out.println("Change from branch 2");
         System.out.println("Líne 3");
         System.out.println("switching to master");
     }
